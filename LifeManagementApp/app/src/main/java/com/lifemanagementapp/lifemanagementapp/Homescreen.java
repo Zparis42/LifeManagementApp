0f4bd40e668bdcc71b1872af7c442b526a07e833
bcopsahl <bcopsahl@mtu.edu>
2017-02-21 11:43:27 -0500

    Added more buttons and icons
    Added buttons to the navigation drawer and gave them placeholder icons.
    Re-added the overflow menu to the homescreen toolbar.

--- LifeManagementApp/app/src/main/java/com/lifemanagementapp/lifemanagementapp/Homescreen.java
+++ LifeManagementApp/app/src/main/java/com/lifemanagementapp/lifemanagementapp/Homescreen.java
@@ -1,6 +1,9 @@
 package com.lifemanagementapp.lifemanagementapp;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -19,6 +23,7 @@ public class Homescreen extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +31,29 @@ public class Homescreen extends AppCompatActivity {
         setContentView(R.layout.activity_homescreen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
         mDrawerToggle = new ActionBarDrawerToggle( this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close );
         mDrawerLayout.setDrawerListener( mDrawerToggle );
         mDrawerToggle.syncState();
 
+        //Removes tint from menu items
+        mNavigationView.setItemIconTintList(null);
+        //Enables the navigation button on the toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_homescreen, menu);
+        getMenuInflater().inflate(R.menu.menu_homescreen, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected( MenuItem item ) {
+        //Opens navigation drawer if navigation button is clicked
         if(mDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }

e253ca9281a164567643f56f438927bbe75732ae
Brady Opsahl <bcopsahl@c28-0117-14.ad.mtu.edu>
2017-02-26 13:27:30 -0500

    Navigation to other activites now working, check the bottom of Homescreen.java for more info.

---
+++ AndroidStudioProjects/LifeManagementApp-master/LifeManagementApp/app/src/main/java/com/lifemanagementapp/lifemanagementapp/Homescreen.java
@@ -0,0 +1,114 @@
+package com.lifemanagementapp.lifemanagementapp;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+public class Homescreen extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+
+        // Creates new toolbar and sets it as the active action bar
+        Toolbar toolbar = (Toolbar) findViewById( R.id.my_toolbar );
+        setSupportActionBar( toolbar) ;
+
+        mNavigationView = (NavigationView) findViewById( R.id.nav_view );
+        mDrawerLayout = (DrawerLayout) findViewById( R.id.nav_drawer );
+        mDrawerToggle = new ActionBarDrawerToggle( this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close );
+        mDrawerLayout.setDrawerListener( mDrawerToggle );
+        mDrawerToggle.syncState( );
+
+        // Removes tint from menu items
+        mNavigationView.setItemIconTintList(null);
+
+        // Enables the navigation button on the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void selectItem(int position) {
+        switch(position) {
+            case 1:
+                Intent a = new Intent(Homescreen.this, test_activity.class);
+                startActivity(a);
+                break;
+            case 2:
+                Intent b = new Intent(Homescreen.this, test_activity.class);
+                startActivity(b);
+                break;
+            default:
+        }
+    }
+
+    // Inflate the menu; this adds items to the action bar if it is present
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_homescreen, menu);
+        return true;
+    }
+
+    // Opens navigation drawer if navigation button is clicked
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
+    // Starts Activity based on whatever menu item is selected from navigation drawer
+    public void onNavMenuItemClick( MenuItem item ) {
+
+        switch ( item.getItemId( ) ) {
+            case R.id.action_calendar:
+                Intent a = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( a );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_health:
+                Intent b = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( b );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_budget:
+                Intent c = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( c );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_goals:
+                Intent d = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( d );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_settings:
+                Intent e = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( e );
+                mDrawerLayout.closeDrawer( mNavigationView );
+        }
+    }
+}
---
+++ AndroidStudioProjects/LifeManagementApp-master/README.md
@@ -0,0 +1,11 @@
+# LifeManagementApp
+The life management app is designed to (given your input in these areas) help plan how much money you should spend on various 
+aspects of life, keep track of important events, remind the user of medications they  must take, deadlines to meet, etc.
+as well as keep track of goals and accomplished goals.
+
+HOW TO USE:
+1. To access settings, click the 3 dots in the top right corner of the home screen. This also accesses extra features (once added)
+2. To access main parts of the app, click the 3 bars in the top left corner of the home screen. This will bring up a navigation
+menu with the main portions of the app included. Once these portions are implemented, clicking on them will bring the user
+to the page for that portion.
+

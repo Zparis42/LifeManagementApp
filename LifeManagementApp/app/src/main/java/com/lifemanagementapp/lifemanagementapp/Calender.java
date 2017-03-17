fe7c47a443d382a160d50ace19ca5a000bd4dae5
Kevin Usher <kddavis@c28-0117-15.ad.mtu.edu>
2017-03-17 13:35:56 -0400

    Added calendar event

---
+++ LifeManagementApp/app/src/main/java/com/lifemanagementapp/lifemanagementapp/Calender.java
@@ -0,0 +1,74 @@
+package com.lifemanagementapp.lifemanagementapp;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.*;
+import android.view.*;
+import android.widget.*;
+import com.lifemanagementapp.*;
+import android.widget.CalendarView.*;
+import android.widget.Toast;
+
+
+
+public class Calender extends ActionBarActivity {
+
+    private static final long double_Click_Time = 250; // in millis
+    private long lastPressTime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+
+        final CalendarView calendarView=(CalendarView)findViewById(R.id.action_calendar);
+        calendarView.setClickable(true);
+        ExpandableListView mExpandableListView = (ExpandableListView)findViewById(R.id.action_calendar_event);
+        //mExpandableListView.expandGroup(groupPosition);
+        calendarView.setOnDateChangeListener(new OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                long pressTime = System.currentTimeMillis();
+                if (pressTime - lastPressTime <= double_Click_Time) {
+                    String message = "Double Click";
+                    Toast.makeText(getApplicationContext(), message,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    String message = String.valueOf(month + 1) + "-" + String.valueOf(dayOfMonth) + "-" + String.valueOf(year);
+                    Toast.makeText(getApplicationContext(), message,
+                            Toast.LENGTH_LONG).show();
+                }
+                lastPressTime = pressTime;
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calender, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- LifeManagementApp/app/src/main/java/com/lifemanagementapp/lifemanagementapp/Homescreen.java
+++ LifeManagementApp/app/src/main/java/com/lifemanagementapp/lifemanagementapp/Homescreen.java
@@ -29,31 +29,50 @@ public class Homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
-        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
+
+        // Creates new toolbar and sets it as the active action bar
+        Toolbar toolbar = (Toolbar) findViewById( R.id.my_toolbar );
+        setSupportActionBar( toolbar) ;
+
+        mNavigationView = (NavigationView) findViewById( R.id.nav_view );
+        mDrawerLayout = (DrawerLayout) findViewById( R.id.nav_drawer );
         mDrawerToggle = new ActionBarDrawerToggle( this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close );
         mDrawerLayout.setDrawerListener( mDrawerToggle );
-        mDrawerToggle.syncState();
+        mDrawerToggle.syncState( );
 
-        //Removes tint from menu items
+        // Removes tint from menu items
         mNavigationView.setItemIconTintList(null);
-        //Enables the navigation button on the toolbar
+
+        // Enables the navigation button on the toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
+    private void selectItem(int position) {
+        switch(position) {
+            case 1:
+                Intent a = new Intent(Homescreen.this, test_activity.class);
+                startActivity(a);
+                break;
+            case 2:
+                Intent b = new Intent(Homescreen.this, test_activity.class);
+                startActivity(b);
+                break;
+            default:
+        }
+    }
+
+    // Inflate the menu; this adds items to the action bar if it is present
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_homescreen, menu);
         return true;
     }
 
+    // Opens navigation drawer if navigation button is clicked
     @Override
     public boolean onOptionsItemSelected( MenuItem item ) {
-        //Opens navigation drawer if navigation button is clicked
+
         if(mDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }
@@ -62,4 +81,34 @@ public class Homescreen extends AppCompatActivity {
         }
     }
 
+    // Starts Activity based on whatever menu item is selected from navigation drawer
+    public void onNavMenuItemClick( MenuItem item ) {
+
+        switch ( item.getItemId( ) ) {
+            case R.id.action_calendar:
+                Intent a = new Intent( Homescreen.this, Calender.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( a );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_health:
+                Intent b = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( b );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_budget:
+                Intent c = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( c );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_goals:
+                Intent d = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( d );
+                mDrawerLayout.closeDrawer( mNavigationView );
+
+            case R.id.action_settings:
+                Intent e = new Intent( Homescreen.this, test_activity.class ); // Change "test_activity.class" to whatever class this should link to
+                startActivity( e );
+                mDrawerLayout.closeDrawer( mNavigationView );
+        }
+    }
 }
